d09f202263247b919788976806656b13f6002ca3
RichaLimbachiya <richalimbachiya2004@gmail.com>
2025-05-15 10:58:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-const parseCode = (str) => {
-  // your code here
-};
+function parseCode(str) {
+  // Split the string on one or more zeros using regex
+  const parts = str.split(/0+/);
 
-// Do not change the code below
-const str = prompt("Enter str: ");
-alert(JSON.stringify(parseCode(str)));
+  // Return the object with extracted parts
+  return {
+    firstName: parts[0],
+    lastName: parts[1],
+    id: parts[2]
+  };
+}
